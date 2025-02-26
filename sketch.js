36833ca79e3cc7dd4fcaa7aa351813ab57eb17a7
OmarEleno <l23170284@Culiacan.tecnm.mx>
2025-02-26 22:30:32 +0000

    Final

--- sketch.js
+++ sketch.js
@@ -45,19 +45,22 @@ function draw() {
  strokeWeight(4);
  stroke(0);
  for(let i=0; i<puntos; i++){
-  let x = random(-radio, radio);
+  let x = random(-width/2, width/2);
   let y = random(-height/2, height/2);
   point(x,y);
-  let d = dist(0,0,x,y);
+  let d = distancia(0,0,x,y);
   if(d<=radio){
     contador++;
   }
   let pi = (contador*lado*lado)/(radio*radio*puntos);
   fill(255);
   textSize(28);
-  text("Dentro: "+contador, 150, 330);
-  text("Total: "+puntos, 150, 360);
-  text("Pi: "+pi, 150, 390);
+  text("Dentro: "+contador,-150, 330);
+  text("Total: "+puntos, -150, 360);
+  text("Pi: "+pi, -150, 390);
+ }
 
+ function distancia(x1, y1, x2, y2){
+  return sqrt(pow(x2-x1,2)+pow(y2-y1,2));
  }
 }
